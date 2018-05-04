0012f6cab381e2e5541cf222b8198a72ec68f4bc
Michel Balthazar <michel@nfe.io>
2018-05-03 21:20:11 -0300

    Create test project to validate methods test

---
+++ StateTaxTests/ScrapTests.cs
@@ -0,0 +1,47 @@
+using System;
+using Xunit;
+using StateTax;
+using System.Threading.Tasks;
+
+namespace StateTaxTests
+{
+    public class ScrapTests
+    {
+        [Fact]
+        public async Task ScrapGet_whenGetIsValid_Return200()
+        {
+            //arrange
+            var scrap = new Scrap();
+            //act
+            var resposta = await scrap.PreScrap();
+            //assert
+            Assert.Equal(200, resposta);
+        }
+        [Fact]
+        public async Task ScrapGet_whenGetIsNotValid_Return400()
+        {
+            //arrange
+            var scrap = new Scrap();
+            scrap.UrlGet = "http://www.xablau.org";
+            //act
+            var resposta = await scrap.PreScrap();
+            //assert
+            Assert.NotNull(resposta);
+            Assert.Equal(400, resposta);
+        }
+        [Fact]
+        public async Task ScrapPost_whenPostIsValid_Return200()
+        {
+            //arrange
+            var scrap = new Scrap();
+            var respostaPost = "09044235000150";
+
+            //act
+            var resp = await scrap.ScrapPost(respostaPost);
+
+            //assert
+            Assert.NotNull(resp);
+            //Assert.Equal(resp, respostaPost);
+        }
+    }
+}
