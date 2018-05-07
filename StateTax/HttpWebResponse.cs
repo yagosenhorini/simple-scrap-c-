df19ba20d1f3a007c53fa925a81cc45136e568d3
yagosenhorini <yagosenhorini30@gmail.com>
2018-05-07 16:36:43 -0300

    Insert new variables on Get Method for list all the header content

--- StateTax/HttpWebResponse.cs
+++ StateTax/HttpWebResponse.cs
@@ -13,6 +13,9 @@ namespace StateTax
         {
             var request = (HttpWebRequest)WebRequest.Create(uri);
             request.AutomaticDecompression = DecompressionMethods.GZip | DecompressionMethods.Deflate;
+            request.UserAgent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/66.0.3359.139 Safari/537.36";
+            request.KeepAlive = true;
+            request.Accept = "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8";
 
             using (var response = await request.GetResponseAsync())
             using (var stream = response.GetResponseStream())
