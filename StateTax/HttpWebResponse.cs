6f7b7f7ea69651f59ecd65d3ae6c65d793bad48e
yagosenhorini <yagosenhorini30@gmail.com>
2018-05-17 13:24:13 -0300

    Insert new headers

--- StateTax/HttpWebResponse.cs
+++ StateTax/HttpWebResponse.cs
@@ -9,13 +9,13 @@ namespace StateTax
 {
    public static class HttpWebResponse
     {
-        public static async Task<string> GetAsync(string uri)
+        public static async Task<string> GetAsync(string uri, string method="GET")
         {
             var request = (HttpWebRequest)WebRequest.Create(uri);
             request.AutomaticDecompression = DecompressionMethods.GZip | DecompressionMethods.Deflate;
             request.UserAgent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/66.0.3359.139 Safari/537.36";
-            request.KeepAlive = true;
             request.Accept = "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8";
+            request.Method = method;
 
             using (var response = await request.GetResponseAsync())
             using (var stream = response.GetResponseStream())
@@ -30,10 +30,20 @@ namespace StateTax
             byte[] dataBytes = Encoding.UTF8.GetBytes(data);
 
             var request = (HttpWebRequest)WebRequest.Create(uri);
+            Scrap scrap = new Scrap();
+            var referencia = scrap.UrlGet.Replace("/ccn", "");
+            request.Headers["Host"] = referencia;
             request.AutomaticDecompression = DecompressionMethods.GZip | DecompressionMethods.Deflate;
             request.ContentLength = dataBytes.Length;
             request.ContentType = contentType;
+            request.Headers["Origin"] = referencia;
+            request.Headers["Cache-Control"] = "max-age=0";
+            request.Headers["Accept-Language"] = "en-US,en;q=0.9";
+            request.Headers["Upgrade-Insecure-Requests"] = "1";
+            request.Headers["Referer"] = scrap.UrlGet;
             request.Method = method;
+            request.UserAgent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/66.0.3359.139 Safari/537.36";
+            request.Accept = "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8";
 
             using (var requestBody = request.GetRequestStream())
             {
