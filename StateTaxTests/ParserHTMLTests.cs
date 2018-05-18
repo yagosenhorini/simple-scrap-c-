4e3ac607d021adaf531138f05d24fb6fb896a0d8
yagosenhorini <yagosenhorini30@gmail.com>
2018-05-17 21:31:26 -0300

    IMplements test class to test the parser method

---
+++ StateTaxTests/ParserHTMLTests.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Xunit;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using StateTax;
+using System.IO;
+
+namespace StateTaxTests
+{
+    public class ParserHTMLTests
+    {
+        [Fact]
+        public async Task ParserHTML_whenHTMLIsAvailable_returnOk()
+        {
+            ScrapParser scrapParser = new ScrapParser();
+            var doc = File.ReadAllText(@"Files\HTML.txt");
+            var ret = await scrapParser.ParseHTML(doc);
+            var tabela = "table";
+            Assert.Contains(tabela, doc);
+            //var ret = scrapParser.ParseHTML(html).Result;
+            //Se o método for async, usar o await, senão o metodo será void e o retorno deverá ter Result;
+
+        }
+    }
+}
