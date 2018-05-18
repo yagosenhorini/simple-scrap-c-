ce482bba64dfd76b5fd79da7dd9ab5787b31e4ab
yagosenhorini <yagosenhorini30@gmail.com>
2018-05-17 21:29:28 -0300

    Implement new class to extract only the necessary part of web page

---
+++ StateTax/ScrapParser.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StateTax
+{
+    public class ScrapParser
+    {
+        public async Task<string> ParseHTML(string parser)
+        {
+            var indexInicio = parser.IndexOf("<table ");
+            var indexFim = parser.IndexOf("</table>");
+            var indexResult = indexFim - indexInicio;
+            return parser.Substring(indexInicio, indexResult);
+        }
+    }
+}
